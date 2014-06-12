60f515a08a2fd626f92d38a76a66d909f0770cc7
Pascal <pascalworek@gmail.com>
2014-06-12 12:27:29 +0200

    categorien WIP

--- SMPT/SMPT/DrinksListViewController.m
+++ SMPT/SMPT/DrinksListViewController.m
@@ -102,6 +102,12 @@
     }
 
 }
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSString *sectionName;
+    sectionName =     return sectionName;
+}
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
