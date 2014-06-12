fbeabe372936cc96f0a85eb80a0fcaacef45abf3
Anh <anh1503@hotmail.com>
2014-06-12 12:22:58 +0200

    beaconviewcontroller, category

---
+++ SMPT/DrinksCategory.m
@@ -0,0 +1,13 @@
+//
+//  DrinksCategory.m
+//  SMPT
+//
+//  Created by Anh Tran on 12-06-14.
+//  Copyright (c) 2014 Fontys. All rights reserved.
+//
+
+#import "DrinksCategory.h"
+
+@implementation DrinksCategory
+
+@end
--- SMPT/SMPT/DrinksListViewController.m
+++ SMPT/SMPT/DrinksListViewController.m
@@ -9,6 +9,7 @@
 #import "DrinksListViewController.h"
 #import "TableCell.h"
 #import "CurrentDrinksViewController.h"
+#import "DrinksCategory.h"
 
 @interface DrinksListViewController ()
 
@@ -83,6 +84,7 @@
     [formatter setLocale:[[NSLocale alloc]initWithLocaleIdentifier:@"nl_NL" ]];
     cell.lblDrinksPrice.text=[formatter stringFromNumber: drinksPrice[row]];
    // cell.lblDrinksPrice.text = [NSString stringWithFormat:@"%@", drinksPrice[row]];
+    
     return cell;
 }
 
