b35e60be7ef5e9d871e1e44903cff594a12f0a79
Pascal <pascalworek@gmail.com>
2014-06-26 11:12:08 +0200

    yadayada

--- SMPT/SMPT/ViewGeldAnimatie.m
+++ SMPT/SMPT/ViewGeldAnimatie.m
@@ -68,12 +68,12 @@
         [alert show];
         
             }
-    if(--seconds ==2){
+    if(seconds ==2){
      //   [NSTimer scheduledTimerWithTimeInterval:1.4 target:self selector:@selector(closeAlert) userInfo:nil repeats:NO];
         [alert dismissWithClickedButtonIndex:0 animated:YES];
 
     }
-    if(--seconds == 0){
+    if(seconds == 0){
         [timer invalidate];
         [self dismissViewControllerAnimated:NO completion:self.homeBlock];
         
